5fdd0bf26c888ef74a59a5c70eb8264b050e8ae9
cg <31290848+slightlyskepticalpotat@users.noreply.github.com>
2021-05-20 23:52:00 -0400

    w

--- snakewhisper_chat.py
+++ snakewhisper_chat.py
@@ -32,7 +32,7 @@ class Server(threading.Thread):
     def accept_connection(self):
         """Accepts a connection and does key exchange."""
         try:
-            peer_public_key = load_pem_public_key(self.incoming.recv(4096))
+            peer_public_key = load_pem_public_key(self.peer.recv(4096))
             self.peer.sendall(private_key.public_key().public_bytes(
                 Encoding.PEM, PublicFormat.SubjectPublicKeyInfo))
             shared_key = private_key.exchange(ec.ECDH(), peer_public_key)
